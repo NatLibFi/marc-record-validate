7856ab386ceee52d56444f87ae0075a11d8015a2
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-01-23 13:45:56 +0200

    Change license to LGPL and update copyright years

--- src/index.spec.js
+++ src/index.spec.js
@@ -4,21 +4,21 @@
 *
 * Validate and fix MARC records
 *
-* Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+* Copyright (c) 2014-2019 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of marc-record-validate
 *
 * marc-record-validate program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
+* it under the terms of the GNU Lesser General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
 * marc-record-validate is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
+* GNU Lesser General Public License for more details.
 *
-* You should have received a copy of the GNU Affero General Public License
+* You should have received a copy of the GNU Lesser General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 * @licend  The above is the entire license notice
