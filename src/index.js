c795dd3c170e786a3b003aac64cb5905cac24edb
Olli Pajuluoma <36146641+OlliPaj@users.noreply.github.com>
2019-12-17 14:09:23 +0200

    Do not return undefined message property  (#7)

--- src/index.js
+++ src/index.js
@@ -52,41 +52,42 @@ export default function (validators = []) {
 				valid: results.every(r => r.state !== 'invalid')
 			};
 
+			function getResponse(validator, messages) {
+				const response = {
+					description: validator.description,
+					state: 'invalid'
+				};
+
+				if (typeof (messages) !== 'undefined') {
+					response.messages = messages;
+				}
+
+				return response;
+			}
+
 			async function iterate(pendingValidators, results = []) {
 				const validator = pendingValidators.shift();
 
 				if (validator) {
 					const {valid, messages} = await validator.validate(record);
+					const response = getResponse(validator, messages);
 
 					if (valid) {
-						return iterate(pendingValidators, results.concat({
-							description: validator.description,
-							state: 'valid'
-						}));
+						response.state = 'valid';
+						return iterate(pendingValidators, results.concat(response));
 					}
 
 					if (fix && validator.fix) {
 						await validator.fix(record);
-						return iterate(pendingValidators, results.concat({
-							description: validator.description,
-							state: 'fixed',
-							messages
-						}));
+						response.state = 'fixed';
+						return iterate(pendingValidators, results.concat(response));
 					}
 
 					if (failOnError) {
-						return results.concat({
-							description: validator.description,
-							state: 'invalid',
-							messages
-						});
+						return results.concat(response);
 					}
 
-					return iterate(pendingValidators, results.concat({
-						description: validator.description,
-						state: 'invalid',
-						messages
-					}));
+					return iterate(pendingValidators, results.concat(response));
 				}
 				return results;
 			}
@@ -97,28 +98,18 @@ export default function (validators = []) {
 
 				if (validator) {
 					const {valid, messages} = await validator.validate(record);
+					const response = getResponse(validator, messages);
 
 					if (valid) {
-						return iterateValidate(pendingValidators, pendingResults, results.concat({
-							description: validator.description,
-							state: originalResult.state === 'fixed' ? 'fixed' : 'valid',
-							messages
-						}));
+						response.state = originalResult.state === 'fixed' ? 'fixed' : 'valid';
+						return iterateValidate(pendingValidators, pendingResults, results.concat(response));
 					}
 
 					if (failOnError) {
-						return results.concat({
-							description: validator.description,
-							state: 'invalid',
-							messages
-						});
+						return results.concat(response);
 					}
 
-					return iterateValidate(pendingValidators, pendingResults, results.concat({
-						description: validator.description,
-						state: 'invalid',
-						messages
-					}));
+					return iterateValidate(pendingValidators, pendingResults, results.concat(response));
 				}
 				return results;
 			}
